80317b6d8d21792ed689d594636d347d807f5ccc
BranimirE <brani.espinoza@gmail.com>
2014-04-14 01:52:45 -0400

    myemacsconfigurations

--- uptogit.bash
+++ uptogit.bash
@@ -1,3 +1,6 @@
+#Ahora puedo subir los archivos a github mas facilmente B°)
+#Pone como mensaje del commit el nombre de la carpeta que contiene a los archivos
+#Copiar o crear un enlace simbolico en /bin/ para poder ejecutar en cualquier carpeta
 git add $*
 message=$(pwd | awk 'BEGIN {FS="/"}; {print $NF}')
 git commit -m $message
